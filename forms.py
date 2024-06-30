04e05cc475172eb2ca172d01b757c7095e63631c
Vronst <vronst@gmail.com>
2024-06-30 21:15:46 +0200

    forms rendered

---
+++ forms.py
@@ -0,0 +1,26 @@
+from wtforms import StringField, SubmitField, EmailField, PasswordField
+from wtforms.validators import DataRequired, Email, Length
+from flask_wtf import FlaskForm
+from flask_ckeditor import CKEditorField
+
+
+class LoginForm(FlaskForm):
+    email = EmailField(label='Email: ', validators=[DataRequired()])
+    password = PasswordField(label='Password: ', validators=[DataRequired()])
+    submit = SubmitField()
+    
+
+class RegisterForm(FlaskForm):
+    nick = StringField(label='Nick: ', validators=[DataRequired(),
+                                                   Length(min=4, max=10, message="Nick must be 4-10 letter long")])
+    email = EmailField(label='Email: ', validators=[DataRequired(),
+                                                    Email("Invalid email address")])
+    password = PasswordField(label='Password: ', validators=[DataRequired(),
+                                                             Length(min=8, message="Password must be at least 8 letters long")])
+    submit = SubmitField("Register")
+    
+
+class ToDoForm(FlaskForm):
+    task = StringField(label='Task: ', validators=[DataRequired()])
+    task_description = CKEditorField(label='Description: ')   
+    submit = SubmitField()
--- main.py
+++ main.py
@@ -1,12 +1,47 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect, url_for
 import smtplib
+import os
+from forms import RegisterForm, LoginForm, ToDoForm
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    if 'user' in session:
+        return render_template('index.html', home=True)
+    else:
+        return render_template('index.html', text="You need to be logged in to acces ToDO list!", home=True)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    # blocks logged users from registering
+    if 'user' in session:
+        return redirect(url_for('index'))
+    form = RegisterForm()
+    
+    if form.validate_on_submit():
+        name = form.nick.data
+        print(name)
+        # TODO: base with users to check for duplicates before registering
+        return "123456"
+    else:
+        print(form.errors)
+    return render_template('register.html', form=form)
+
+
+@app.route('/login')
+def login():
+    # blocks logged users from logging again
+    form = LoginForm()
+    if 'user' in session:
+        return redirect(url_for('index'))
+    return render_template('login.html', form=form)
 
 
 if __name__ == '__main__':
